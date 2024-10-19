6fdd3b6e7e47e6725d831cda38d25026a569766d
NamespaceV <s-zephyr@o2.pl>
2024-10-19 22:31:11 +0200

    move Enemies

--- Assets/Gameplay/Manager/SingleGameRun.cs
+++ Assets/Gameplay/Manager/SingleGameRun.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Unity.VisualScripting;
+using UnityEditor.Build.Content;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -26,14 +28,15 @@ namespace Assets.Gameplay.Manager
             {
                 if (tile.GetTileType() == TileType.HERO)
                 {
-                    if (_playerCoords != null) {
-                        Debug.LogWarning("Multiple Players not supported yet "+_playerCoords);
+                    if (_playerCoords != null)
+                    {
+                        Debug.LogWarning("Multiple Players not supported yet " + _playerCoords);
                     }
                     _playerCoords = tile.GetCoords();
                 }
                 else if (tile.GetTileType() == TileType.ENEMY)
                 {
-                    _enemies.Add(new EnemyState(_mgr, tile.GetCoords()));
+                    _enemies.Add(new EnemyState(_mgr, this, tile.GetCoords()));
                 }
             }
             _mgr.GetGrid().GetTile(_playerCoords.Value).SetSelected(true);
@@ -49,30 +52,118 @@ namespace Assets.Gameplay.Manager
             var adjacentTile = g.GetAdjacentTile(_playerCoords.Value, dir.Value);
 
             if (adjacentTile?.GetTileType() == TileType.EMPTY
-                    && playerTile.AllowsMove(dir.Value)) {
+                    && playerTile.AllowsMove(dir.Value))
+            {
                 playerTile.SetTileType(TileType.EMPTY);
                 playerTile.SetSelected(false);
                 adjacentTile.SetTileType(TileType.HERO);
                 adjacentTile.SetSelected(true);
                 _playerCoords = adjacentTile.GetCoords();
+
+                moveEnemies();
             }
 
         }
 
+        private void moveEnemies()
+        {
+            foreach (var e in _enemies) {
+                e.Move();
+            }
+        }
+
         public void HandleClick(Vector2Int coord, PointerEventData.InputButton button)
         {
+            //do nothing
+        }
+
+        public void PlayerLost()
+        {
+            _gameEnded = true;
         }
     }
 
     public class EnemyState
     {
-        private GameManager mgr;
-        private Vector2Int coord;
+        private GameManager _mgr;
+        private SingleGameRun _currentRun;
+        private Vector2Int _coord;
 
-        public EnemyState(GameManager mgr, Vector2Int coord)
+        private bool _lastSeenInCurrentTurn;
+        private bool _lastSeenPursueActive;
+        private Vector2Int _lastSeenCoord;
+        private Direction _lastSeenDirection;
+        private int _lastSeenDistance;
+
+        public EnemyState(GameManager mgr, SingleGameRun currentRun, Vector2Int coord)
         {
-            this.mgr = mgr;
-            this.coord = coord;
+            _mgr = mgr;
+            _currentRun = currentRun;
+            _coord = coord;
+        }
+
+        private Tile _myTile => _mgr.GetGrid().GetTile(_coord);
+
+        public void Move()
+        {
+            _lastSeenInCurrentTurn = false;
+
+            var tile = _myTile;
+            for (int dir = 0; dir < 4; ++dir)
+            {
+                var d = (Direction)dir;
+                if (!tile.AllowsMove(d)) { continue; }
+                var adjacent = _mgr.GetGrid().GetAdjacentTile(_coord, d);
+                if (adjacent?.GetTileType() == TileType.HERO)
+                {
+                    _currentRun.PlayerLost();
+                    moveTo(adjacent);
+                    return;
+                }
+                if (adjacent?.GetTileType() == TileType.EMPTY)
+                {
+                    seekPlayer(adjacent, d);
+                }
+            }
+            if (_lastSeenPursueActive)
+            {
+                var adjacent = _mgr.GetGrid().GetAdjacentTile(_coord, _lastSeenDirection);
+
+                if (adjacent.GetTileType() != TileType.EMPTY) { return; }
+
+                moveTo(adjacent);
+
+                _lastSeenDistance -= 1;
+                if (_lastSeenDistance == 0)
+                {
+                    _lastSeenPursueActive = false;
+                }
+            }
+        }
+
+        private void seekPlayer(Tile tile, Direction d)
+        {
+            var distance = 1;
+            while (tile && tile.AllowsMove(d))
+            {
+                distance += 1;
+                tile = _mgr.GetGrid().GetAdjacentTile(tile.GetCoords(), d);
+                if (tile?.GetTileType() == TileType.HERO)
+                {
+                    _lastSeenPursueActive = true;
+                    _lastSeenInCurrentTurn = true;
+                    _lastSeenDirection = d;
+                    _lastSeenDistance = distance;
+                    _lastSeenCoord = tile.GetCoords();
+                }
+            }
+        }
+
+        private void moveTo(Tile adjacent)
+        {
+            _myTile.SetTileType(TileType.EMPTY);
+            adjacent.SetTileType(TileType.ENEMY);
+            _coord = adjacent.GetCoords();
         }
     }
 }
